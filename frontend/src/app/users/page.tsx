02fb010139a4ccebe0b974a127cafbb49da76d6c
MARCIO TORRES RODRIGUES DE SOUZA <0031432412031@lti.fatecso.edu.br>
2025-03-21 18:02:03 -0300

    frontend
    update

---
+++ frontend/src/app/users/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useState} from "react";
+import userService, {User} from "@/services/userService";
+
+export default function UsersPage(){
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const data = await userService. getUsers();
+                setUsers(data);
+            } catch (error) {
+                console.log("Erro ao buscar usuários", error);
+            }
+        }
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista de Usuários</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
---
+++ frontend/src/services/userService.ts
@@ -0,0 +1,39 @@
+export interface User {
+    id?: number;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    password?: string;
+    photo: string;
+}
+
+const fakeUser: User[] = [
+    {    
+        id: 1,
+        name: "João da Silva",
+        email: "joaodasilva@email.com",
+        phone: "(15) 12312-6456",
+        address: "Rua do Ninho Verde, 679",
+        password: "senhaHashed",
+        photo: "user.png"
+    },
+    {
+        id: 2,
+        name: "thiago da Silva",
+        email: "thiagodasilva@email.com",
+        phone: "(15) 12312-6456",
+        address: "Rua do Ninho Vermelho, 340",
+        password: "senhaHashed",
+        photo: "user.png"
+    }
+]
+
+
+const userService = {
+    async getUsers() : Promise<User[]> {
+        return fakeUser
+    }
+}
+
+export default userService;
