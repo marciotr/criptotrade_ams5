23636e29603752b69daaa1a8595ba01fee721c67
cauanotz <cauanortiz2019@gmail.com>
2025-05-30 23:23:15 -0300

    feat(.bat): bat adicionado para inicialização mais rápida das api's e frontend

---
+++ iniciador_apis.bat
@@ -0,0 +1,44 @@
+@echo off
+
+cd /d %~dp0
+echo Iniciando APIs...
+
+echo.
+echo CryptoApi...
+cd .\backend\cryptoApi
+start dotnet run
+cd ..\..
+
+echo.
+echo CurrencyAvailables...
+cd .\backend\currencyAvailables
+start dotnet run
+cd ..\..
+
+echo.
+echo UserApi...
+cd .\backend\userApi
+start dotnet run
+cd ..\..
+
+echo.
+echo WalletApi...
+cd .\backend\walletApi
+start dotnet run
+cd ..\..
+
+echo.
+echo Frontend...
+cd /d %~dp0\frontend
+
+if not exist node_modules (
+    echo Instalando dependências do Frontend...
+    npm install
+)
+
+echo Iniciando Frontend...
+start "Frontend" cmd /k "cd /d %~dp0\frontend && npm run dev"
+
+echo.
+echo Todas as APIs e o Frontend foram iniciados.
+pause
